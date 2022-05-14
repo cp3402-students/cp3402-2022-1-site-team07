822b9d27a3f6d2a4da446a026c9c974d098a7503
Storm Jones <9743815+StormJones@users.noreply.github.com>
2022-05-14 13:06:12 +1000

    Installed ACF & Created Editable Front Page
    Installed plugin Advanced Custom Fields to allow for editing across the home page's various fields. Additionally made the home page editable.

--- Website/wp-content/themes/team-07-theme/front-page.php
+++ Website/wp-content/themes/team-07-theme/front-page.php
@@ -16,22 +16,39 @@
 <title>Townsville Jazz Club</title>
 </head>
 <body>
-<?php require 'header.php'; ?>
+
+
+<header>
+  <?php require 'header.php'; ?>
+  <div id="banner" class="card "> <img class="card-img img-fluid " src="<?php echo get_bloginfo('template_directory'); ?>/Images/the-crowd-at-fish-inn.jpg" alt="banner">
+    <div class="card-img-overlay text-white  d-flex align-items-center justify-content-center align-items-end overlay-dark">
+      <div class="col-lg-6  col-md-12 d-md-block d-none my-4 p-5">
+        <div class="card-body">
+          <h2 class="card-title text-center"><?php echo get_field_object( 'spotlight_header_text' )['value']; ?></h2>
+          <hr class="line">
+          <p class="card-text text-center"><?php echo get_field_object( 'spotlight_paragraph_text' )['value']; ?>
+            <a class="btn btn-outline-warning text-center btn-lg">Learn more</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+
 <div class="container-fluid bg-light py-5">
   <div class="container">
     <div class="row ">
       <div class="col-4 text-center"> <i class="fa-solid fa-map-location-dot  fa-5x"></i>
         <h3 class="pt-3">Where?</h3>
-        <p>The Fish Inn on The Stran<br>
-          (near the Rockpool) </p>
+        <p><?php echo get_field_object( 'underspotlight_where' )['value']; ?></p>
       </div>
       <div class="col-4 text-center"> <i class="fa-solid fa-clock fa-5x"></i>
         <h3 class="pt-3">Time?</h3>
-        <p>Sunday 5.00pm till 8.00pm </p>
+        <p><?php echo get_field_object( 'underspotlight_time' )['value']; ?></p>
       </div>
       <div class="col-4 text-center"> <i class="fa-solid fa-calendar-week fa-5x"></i>
         <h3 class="pt-3">Date?</h3>
-        <p>Check the Program page for dates </p>
+        <p><?php echo get_field_object( 'underspotlight_date' )['value']; ?></p>
       </div>
     </div>
   </div>
@@ -40,10 +57,8 @@
   <div class="row py-5">
     <div class="col-md-6 mb-3"> <img class="img-fluid" src="<?php echo get_bloginfo('template_directory'); ?>/Images/Anthony-and-the-Gentlemen-of-Jazz-.jpg" alt="Anthony-and-the-Gentlemen-of-Jazz"> </div>
     <div class="col-md-6 mb-3">
-      <h2>Come &amp; enjoy our regular jazz nights</h2>
-      <p>Our friendly and relaxed club has a strong history of supporting jazz in Townsville and has made a great contribution to the Townsville Arts and Music Community over many years.  We encourage new guests to become members and to actively participate in the club’s functions. </p>
-      <p> We feature a range of very talented local bands playing great jazz music in a relaxed yet vibrant environment. The regular jazz nights feature a sit-in bracket and dancers are most welcome. Meals and bar <a href="#">facility available</a>.       No need to book. </p>
-    </div>
+		<?php echo the_content(); ?> 
+	</div>
   </div>
 </div>
 <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3766.5214959803016!2d146.81519231490407!3d-19.259674986982752!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6bd5f8da4be3d759%3A0x942c9f7d51144851!2sHotel%20Grand%20Chancellor%20Townsville!5e0!3m2!1sen!2sau!4v1652360379307!5m2!1sen!2sau" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
--- Website/wp-content/themes/team-07-theme/header.php
+++ Website/wp-content/themes/team-07-theme/header.php
@@ -1,5 +1,4 @@
-<header>
-  <nav class="navbar navbar-expand-lg navbar-dark" >
+<nav class="navbar navbar-expand-lg navbar-dark" >
     <div class="container">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbarToggler3"
         aria-controls="myNavbarToggler3" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
@@ -15,18 +14,3 @@
       </div>
     </div>
   </nav>
-  <div id="banner" class="card "> <img class="card-img img-fluid " src="<?php echo get_bloginfo('template_directory'); ?>/Images/the-crowd-at-fish-inn.jpg" alt="banner">
-    <div class="card-img-overlay text-white  d-flex align-items-center justify-content-center align-items-end overlay-dark">
-      <div class="col-lg-6  col-md-12 d-md-block d-none my-4 p-5">
-        <div class="card-body">
-          <h2 class="card-title text-center">Welcome To <br>
-            Townsville Jazz Club</h2>
-          <hr class="line">
-          <p class="card-text text-center">Our friendly and relaxed club has a strong history of supporting jazz in Townsville and has made a great contribution to the Townsville Arts and Music Community over many years.  We encourage new guests to become members and to actively participate in the club’s functions. <br>
-            <br>
-            <a class="btn btn-outline-warning text-center btn-lg">Learn more</a></p>
-        </div>
-      </div>
-    </div>
-  </div>
-</header>

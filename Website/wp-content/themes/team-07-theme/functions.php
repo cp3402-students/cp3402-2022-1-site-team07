0a27dda8a26dca510020717e931fcb6c3778201a
Storm Jones <9743815+StormJones@users.noreply.github.com>
2022-05-20 20:17:18 +1000

    Added Menu Option to WordPress

--- Website/wp-content/themes/team-07-theme/functions.php
+++ Website/wp-content/themes/team-07-theme/functions.php
@@ -1,6 +1,9 @@
 <?php
 	
-	
+	function wpb_custom_new_menu() {
+	  register_nav_menu('my-custom-menu',__( 'My Custom Menu' ));
+	}
+	add_action( 'init', 'wpb_custom_new_menu' );
 	
 	add_theme_support( 'post-thumbnails' );
 	

6c87c3c2079afa311dcdc198f9a1585666b4e510
nishanbe <Msjammsjam1!>
2022-05-21 18:07:05 +1000

    Re-Customise the post type parameters

--- Website/wp-content/themes/team-07-theme2/functions.php
+++ Website/wp-content/themes/team-07-theme2/functions.php
@@ -57,6 +57,7 @@ function my_admin_style() {
 
 
 // Custom port types
+
 function cptui_register_my_cpts() {
 
     /**
@@ -64,37 +65,42 @@ function cptui_register_my_cpts() {
      */
 
     $labels = [
-        "name" => __( "Bands", "team07theme2" ),
-        "singular_name" => __( "Band", "team07theme2" ),
-        "menu_name" => __( "Bands", "team07theme2" ),
-        "all_items" => __( "All Bands", "team07theme2" ),
-        "add_new" => __( "Add New", "team07theme2" ),
-        "add_new_item" => __( "Add New Band", "team07theme2" ),
-        "edit_item" => __( "Edit Band", "team07theme2" ),
-        "new_item" => __( "New BAnd", "team07theme2" ),
-        "view_item" => __( "View Band", "team07theme2" ),
-        "view_items" => __( "View Bands", "team07theme2" ),
-        "search_items" => __( "Search Bands", "team07theme2" ),
-        "not_found" => __( "No Bands found", "team07theme2" ),
-        "not_found_in_trash" => __( "No Bands found in Trash", "team07theme2" ),
-        "parent" => __( "Parent Band", "team07theme2" ),
-        "insert_into_item" => __( "Insert into Band", "team07theme2" ),
-        "uploaded_to_this_item" => __( "Uploaded to this Band", "team07theme2" ),
-        "filter_items_list" => __( "Filter bands list", "team07theme2" ),
-        "items_list_navigation" => __( "Bands list navigation", "team07theme2" ),
-        "items_list" => __( "Bands List", "team07theme2" ),
-        "attributes" => __( "Bands Attributes", "team07theme2" ),
-        "name_admin_bar" => __( "Bands", "team07theme2" ),
-        "item_published" => __( "Band published", "team07theme2" ),
-        "item_published_privately" => __( "Band published privately", "team07theme2" ),
-        "item_reverted_to_draft" => __( "Band reverted to draft", "team07theme2" ),
-        "item_scheduled" => __( "Band scheduled", "team07theme2" ),
-        "item_updated" => __( "Band updagted", "team07theme2" ),
-        "parent_item_colon" => __( "Parent Band", "team07theme2" ),
+        "name" => __( "Bands", "team07theme" ),
+        "singular_name" => __( "Band", "team07theme" ),
+        "menu_name" => __( "Bands", "team07theme" ),
+        "all_items" => __( "All Bands", "team07theme" ),
+        "add_new" => __( "Add new", "team07theme" ),
+        "add_new_item" => __( "Add new Band", "team07theme" ),
+        "edit_item" => __( "Edit Band", "team07theme" ),
+        "new_item" => __( "New Band", "team07theme" ),
+        "view_item" => __( "View Band", "team07theme" ),
+        "view_items" => __( "View Bands", "team07theme" ),
+        "search_items" => __( "Search Bands", "team07theme" ),
+        "not_found" => __( "No Bands found", "team07theme" ),
+        "not_found_in_trash" => __( "No Bands found in trash", "team07theme" ),
+        "parent" => __( "Parent Band:", "team07theme" ),
+        "featured_image" => __( "Featured image for this Band", "team07theme" ),
+        "set_featured_image" => __( "Set featured image for this Band", "team07theme" ),
+        "remove_featured_image" => __( "Remove featured image for this Band", "team07theme" ),
+        "use_featured_image" => __( "Use as featured image for this Band", "team07theme" ),
+        "archives" => __( "Band archives", "team07theme" ),
+        "insert_into_item" => __( "Insert into Band", "team07theme" ),
+        "uploaded_to_this_item" => __( "Upload to this Band", "team07theme" ),
+        "filter_items_list" => __( "Filter Bands list", "team07theme" ),
+        "items_list_navigation" => __( "Bands list navigation", "team07theme" ),
+        "items_list" => __( "Bands list", "team07theme" ),
+        "attributes" => __( "Bands attributes", "team07theme" ),
+        "name_admin_bar" => __( "Band", "team07theme" ),
+        "item_published" => __( "Band published", "team07theme" ),
+        "item_published_privately" => __( "Band published privately.", "team07theme" ),
+        "item_reverted_to_draft" => __( "Band reverted to draft.", "team07theme" ),
+        "item_scheduled" => __( "Band scheduled", "team07theme" ),
+        "item_updated" => __( "Band updated.", "team07theme" ),
+        "parent_item_colon" => __( "Parent Band:", "team07theme" ),
     ];
 
     $args = [
-        "label" => __( "Bands", "team07theme2" ),
+        "label" => __( "Bands", "team07theme" ),
         "labels" => $labels,
         "description" => "",
         "public" => true,
@@ -104,7 +110,7 @@ function cptui_register_my_cpts() {
         "rest_base" => "",
         "rest_controller_class" => "WP_REST_Posts_Controller",
         "rest_namespace" => "wp/v2",
-        "has_archive" => true,
+        "has_archive" => "bands",
         "show_in_menu" => true,
         "show_in_nav_menus" => true,
         "delete_with_user" => false,
@@ -125,6 +131,8 @@ function cptui_register_my_cpts() {
 }
 
 add_action( 'init', 'cptui_register_my_cpts' );
+
+
 function cptui_register_my_cpts_band() {
 
     /**
@@ -132,37 +140,42 @@ function cptui_register_my_cpts_band() {
      */
 
     $labels = [
-        "name" => __( "Bands", "team07theme2" ),
-        "singular_name" => __( "Band", "team07theme2" ),
-        "menu_name" => __( "Bands", "team07theme2" ),
-        "all_items" => __( "All Bands", "team07theme2" ),
-        "add_new" => __( "Add New", "team07theme2" ),
-        "add_new_item" => __( "Add New Band", "team07theme2" ),
-        "edit_item" => __( "Edit Band", "team07theme2" ),
-        "new_item" => __( "New Band", "team07theme2" ),
-        "view_item" => __( "View Band", "team07theme2" ),
-        "view_items" => __( "View Bands", "team07theme2" ),
-        "search_items" => __( "Search Bands", "team07theme2" ),
-        "not_found" => __( "No Bands found", "team07theme2" ),
-        "not_found_in_trash" => __( "No Bands found in Trash", "team07theme2" ),
-        "parent" => __( "Parent Band", "team07theme2" ),
-        "insert_into_item" => __( "Insert into Band", "team07theme2" ),
-        "uploaded_to_this_item" => __( "Uploaded to this Band", "team07theme2" ),
-        "filter_items_list" => __( "Filter bands list", "team07theme2" ),
-        "items_list_navigation" => __( "Bands list navigation", "team07theme2" ),
-        "items_list" => __( "Bands List", "team07theme2" ),
-        "attributes" => __( "Bands Attributes", "team07theme2" ),
-        "name_admin_bar" => __( "Bands", "team07theme2" ),
-        "item_published" => __( "Band published", "team07theme2" ),
-        "item_published_privately" => __( "Band published privately", "team07theme2" ),
-        "item_reverted_to_draft" => __( "Band reverted to draft", "team07theme2" ),
-        "item_scheduled" => __( "Band scheduled", "team07theme2" ),
-        "item_updated" => __( "Band updagted", "team07theme2" ),
-        "parent_item_colon" => __( "Parent Band", "team07theme2" ),
+        "name" => __( "Bands", "team07theme" ),
+        "singular_name" => __( "Band", "team07theme" ),
+        "menu_name" => __( "Bands", "team07theme" ),
+        "all_items" => __( "All Bands", "team07theme" ),
+        "add_new" => __( "Add new", "team07theme" ),
+        "add_new_item" => __( "Add new Band", "team07theme" ),
+        "edit_item" => __( "Edit Band", "team07theme" ),
+        "new_item" => __( "New Band", "team07theme" ),
+        "view_item" => __( "View Band", "team07theme" ),
+        "view_items" => __( "View Bands", "team07theme" ),
+        "search_items" => __( "Search Bands", "team07theme" ),
+        "not_found" => __( "No Bands found", "team07theme" ),
+        "not_found_in_trash" => __( "No Bands found in trash", "team07theme" ),
+        "parent" => __( "Parent Band:", "team07theme" ),
+        "featured_image" => __( "Featured image for this Band", "team07theme" ),
+        "set_featured_image" => __( "Set featured image for this Band", "team07theme" ),
+        "remove_featured_image" => __( "Remove featured image for this Band", "team07theme" ),
+        "use_featured_image" => __( "Use as featured image for this Band", "team07theme" ),
+        "archives" => __( "Band archives", "team07theme" ),
+        "insert_into_item" => __( "Insert into Band", "team07theme" ),
+        "uploaded_to_this_item" => __( "Upload to this Band", "team07theme" ),
+        "filter_items_list" => __( "Filter Bands list", "team07theme" ),
+        "items_list_navigation" => __( "Bands list navigation", "team07theme" ),
+        "items_list" => __( "Bands list", "team07theme" ),
+        "attributes" => __( "Bands attributes", "team07theme" ),
+        "name_admin_bar" => __( "Band", "team07theme" ),
+        "item_published" => __( "Band published", "team07theme" ),
+        "item_published_privately" => __( "Band published privately.", "team07theme" ),
+        "item_reverted_to_draft" => __( "Band reverted to draft.", "team07theme" ),
+        "item_scheduled" => __( "Band scheduled", "team07theme" ),
+        "item_updated" => __( "Band updated.", "team07theme" ),
+        "parent_item_colon" => __( "Parent Band:", "team07theme" ),
     ];
 
     $args = [
-        "label" => __( "Bands", "team07theme2" ),
+        "label" => __( "Bands", "team07theme" ),
         "labels" => $labels,
         "description" => "",
         "public" => true,
@@ -172,7 +185,7 @@ function cptui_register_my_cpts_band() {
         "rest_base" => "",
         "rest_controller_class" => "WP_REST_Posts_Controller",
         "rest_namespace" => "wp/v2",
-        "has_archive" => true,
+        "has_archive" => "bands",
         "show_in_menu" => true,
         "show_in_nav_menus" => true,
         "delete_with_user" => false,
@@ -193,6 +206,8 @@ function cptui_register_my_cpts_band() {
 }
 
 add_action( 'init', 'cptui_register_my_cpts_band' );
+
+
 function cptui_register_my_taxes() {
 
     /**
@@ -200,36 +215,36 @@ function cptui_register_my_taxes() {
      */
 
     $labels = [
-        "name" => __( "Members", "team07theme2" ),
-        "singular_name" => __( "Member", "team07theme2" ),
-        "menu_name" => __( "Members", "team07theme2" ),
-        "all_items" => __( "All Members", "team07theme2" ),
-        "edit_item" => __( "Edit Member", "team07theme2" ),
-        "view_item" => __( "View Member", "team07theme2" ),
-        "update_item" => __( "Update Member name", "team07theme2" ),
-        "add_new_item" => __( "Add new Member", "team07theme2" ),
-        "new_item_name" => __( "New Member name", "team07theme2" ),
-        "parent_item" => __( "Parent Member", "team07theme2" ),
-        "parent_item_colon" => __( "Parent Member:", "team07theme2" ),
-        "search_items" => __( "Search Members", "team07theme2" ),
-        "popular_items" => __( "Popular Members", "team07theme2" ),
-        "separate_items_with_commas" => __( "Separate Members with commas", "team07theme2" ),
-        "add_or_remove_items" => __( "Add or remove Members", "team07theme2" ),
-        "choose_from_most_used" => __( "Choose from the most used Members", "team07theme2" ),
-        "not_found" => __( "No Members found", "team07theme2" ),
-        "no_terms" => __( "No Members", "team07theme2" ),
-        "items_list_navigation" => __( "Members list navigation", "team07theme2" ),
-        "items_list" => __( "Members list", "team07theme2" ),
-        "back_to_items" => __( "Back to Members", "team07theme2" ),
-        "name_field_description" => __( "The name is how it appears on your site.", "team07theme2" ),
-        "parent_field_description" => __( "Assign a parent term to create a hierarchy. The term Jazz, for example, would be the parent of Bebop and Big Band.", "team07theme2" ),
-        "slug_field_description" => __( "The slug is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens.", "team07theme2" ),
-        "desc_field_description" => __( "The description is not prominent by default; however, some themes may show it.", "team07theme2" ),
+        "name" => __( "Members", "team07theme" ),
+        "singular_name" => __( "Member", "team07theme" ),
+        "menu_name" => __( "Members", "team07theme" ),
+        "all_items" => __( "All Members", "team07theme" ),
+        "edit_item" => __( "Edit Member", "team07theme" ),
+        "view_item" => __( "View Member", "team07theme" ),
+        "update_item" => __( "Update Member name", "team07theme" ),
+        "add_new_item" => __( "Add new Member", "team07theme" ),
+        "new_item_name" => __( "New Member name", "team07theme" ),
+        "parent_item" => __( "Parent Member", "team07theme" ),
+        "parent_item_colon" => __( "Parent Member:", "team07theme" ),
+        "search_items" => __( "Search Members", "team07theme" ),
+        "popular_items" => __( "Popular Members", "team07theme" ),
+        "separate_items_with_commas" => __( "Separate Members with commas", "team07theme" ),
+        "add_or_remove_items" => __( "Add or remove Members", "team07theme" ),
+        "choose_from_most_used" => __( "Choose from the most used Members", "team07theme" ),
+        "not_found" => __( "No Members found", "team07theme" ),
+        "no_terms" => __( "No Members", "team07theme" ),
+        "items_list_navigation" => __( "Members list navigation", "team07theme" ),
+        "items_list" => __( "Members list", "team07theme" ),
+        "back_to_items" => __( "Back to Members", "team07theme" ),
+        "name_field_description" => __( "The name is how it appears on your site.", "team07theme" ),
+        "parent_field_description" => __( "Assign a parent term to create a hierarchy. The term Jazz, for example, would be the parent of Bebop and Big Band.", "team07theme" ),
+        "slug_field_description" => __( "The slug is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens.", "team07theme" ),
+        "desc_field_description" => __( "The description is not prominent by default; however, some themes may show it.", "team07theme" ),
     ];
 
 
     $args = [
-        "label" => __( "Members", "team07theme2" ),
+        "label" => __( "Members", "team07theme" ),
         "labels" => $labels,
         "public" => true,
         "publicly_queryable" => true,
@@ -254,5 +269,62 @@ function cptui_register_my_taxes() {
 add_action( 'init', 'cptui_register_my_taxes' );
 
 
-// Fix 404 error for custom posts
-flush_rewrite_rules( false );
+function cptui_register_my_taxes_members() {
+
+    /**
+     * Taxonomy: Members.
+     */
+
+    $labels = [
+        "name" => __( "Members", "team07theme" ),
+        "singular_name" => __( "Member", "team07theme" ),
+        "menu_name" => __( "Members", "team07theme" ),
+        "all_items" => __( "All Members", "team07theme" ),
+        "edit_item" => __( "Edit Member", "team07theme" ),
+        "view_item" => __( "View Member", "team07theme" ),
+        "update_item" => __( "Update Member name", "team07theme" ),
+        "add_new_item" => __( "Add new Member", "team07theme" ),
+        "new_item_name" => __( "New Member name", "team07theme" ),
+        "parent_item" => __( "Parent Member", "team07theme" ),
+        "parent_item_colon" => __( "Parent Member:", "team07theme" ),
+        "search_items" => __( "Search Members", "team07theme" ),
+        "popular_items" => __( "Popular Members", "team07theme" ),
+        "separate_items_with_commas" => __( "Separate Members with commas", "team07theme" ),
+        "add_or_remove_items" => __( "Add or remove Members", "team07theme" ),
+        "choose_from_most_used" => __( "Choose from the most used Members", "team07theme" ),
+        "not_found" => __( "No Members found", "team07theme" ),
+        "no_terms" => __( "No Members", "team07theme" ),
+        "items_list_navigation" => __( "Members list navigation", "team07theme" ),
+        "items_list" => __( "Members list", "team07theme" ),
+        "back_to_items" => __( "Back to Members", "team07theme" ),
+        "name_field_description" => __( "The name is how it appears on your site.", "team07theme" ),
+        "parent_field_description" => __( "Assign a parent term to create a hierarchy. The term Jazz, for example, would be the parent of Bebop and Big Band.", "team07theme" ),
+        "slug_field_description" => __( "The slug is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens.", "team07theme" ),
+        "desc_field_description" => __( "The description is not prominent by default; however, some themes may show it.", "team07theme" ),
+    ];
+
+
+    $args = [
+        "label" => __( "Members", "team07theme" ),
+        "labels" => $labels,
+        "public" => true,
+        "publicly_queryable" => true,
+        "hierarchical" => false,
+        "show_ui" => true,
+        "show_in_menu" => true,
+        "show_in_nav_menus" => true,
+        "query_var" => true,
+        "rewrite" => [ 'slug' => 'members', 'with_front' => true, ],
+        "show_admin_column" => false,
+        "show_in_rest" => true,
+        "show_tagcloud" => false,
+        "rest_base" => "members",
+        "rest_controller_class" => "WP_REST_Terms_Controller",
+        "rest_namespace" => "wp/v2",
+        "show_in_quick_edit" => false,
+        "sort" => false,
+        "show_in_graphql" => false,
+    ];
+    register_taxonomy( "members", [ "band" ], $args );
+}
+add_action( 'init', 'cptui_register_my_taxes_members' );
